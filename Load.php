96b110ad4bbbe1f1a9d30d826ba93a7683631823
Potvin <shawnpots96@gmail.com>
2018-01-17 09:23:16 -0500

    test

--- Load.php
+++ Load.php
@@ -1,11 +1,18 @@
 <?php
 	echo "Hello world!"; 
-
-	$db = new SQLite3('Draftkings.db');
-
-	$results = $db->query('SELECT YearWeek FROM Draftkingsdb');
-	while ($row = $results->fetchArray()) {
-	    var_dump($row);
-	}
+ 
+// Specify your sqlite database name and path //
+$dir = 'sqlite:C:/sqlite/gui/SQLiteStudio/Draftkings.sqlite';
+ 
+// Instantiate PDO connection object and failure msg //
+$dbh = new PDO($dir) or die("cannot open database");
+ 
+// Define your SQL statement //
+$query = "SELECT YearWeek FROM DraftkingsDB";
+ 
+// Iterate through the results and pass into JSON encoder //
+foreach ($dbh->query($query) as $row) {
+	echo json_encode($row[0]);
+}
 
 ?>
