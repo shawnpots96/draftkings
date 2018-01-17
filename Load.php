9c084888e9163af08a1ac29f1a44ee13a82f7eef
Potvin <shawnpots96@gmail.com>
2018-01-17 09:11:59 -0500

    test

--- Load.php
+++ Load.php
@@ -1,3 +1,9 @@
 <?php
 	$myPDO = new PDO('sqlite:/gui/SQLiteStudio/Draftkings.db');
+	
+	$result = $myPDO->query("SELECT YearWeek FROM DraftkingsDB");
+
+	foreach($result as $row) {
+		print $row['YearWeek'] . "\n";
+	}
 ?>
