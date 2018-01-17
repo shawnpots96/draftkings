2c59c372a886af87e1435e6a510d0d18f70f1c5a
Potvin <shawnpots96@gmail.com>
2018-01-17 09:55:19 -0500

    test

--- Load.php
+++ Load.php
@@ -1,18 +1,19 @@
 <?php
 	echo "Hello world!"; 
  
-// Specify your sqlite database name and path //
-$dir = 'sqlite:C:/sqlite/gui/SQLiteStudio/Draftkings.sqlite';
+	/* Specify your sqlite database name and path */
+	$dir = 'sqlite:C:/Users/Potvin/Desktop/draftkings/Draftkings.sqlite';
+	//$dir = 'sqlite:mydir/myDatabase.sqlite';//
  
-// Instantiate PDO connection object and failure msg //
-$dbh = new PDO($dir) or die("cannot open database");
+	/* Instantiate PDO connection object and failure msg */
+	$dbh = new PDO($dir) or die("cannot open database");
  
-// Define your SQL statement //
-$query = "SELECT YearWeek FROM DraftkingsDB";
+	/* Define your SQL statement */
+	$query = "SELECT YearWeek FROM DraftkingsDB";
  
-// Iterate through the results and pass into JSON encoder //
-foreach ($dbh->query($query) as $row) {
-	echo json_encode($row[0]);
-}
+	/* Iterate through the results and pass into JSON encoder */
+	foreach ($dbh->query($query) as $row) {
+		echo json_encode($row[0]);
+	}
 
 ?>
